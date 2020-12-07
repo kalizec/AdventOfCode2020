6c0efcfd09d87c1536e5ca6f01f104f3320531c7
EVIDENT\f.masolijn <masolijn@evident.nl>
2020-12-07 09:43:07 +0100

    Day 7

--- AdventOfCode/AdventOfCode/Days/Day6.cs
+++ AdventOfCode/AdventOfCode/Days/Day6.cs
@@ -6,18 +6,83 @@ namespace AdventOfCode.Days
 {
     public class Day6 : BaseDay
     {
+        public List<HashSet<char>> GroupAnswers { get; }
+        public List<List<HashSet<char>>> PersonAnswers { get; }
+
         public Day6() : base(2020, 6)
         {
+            this.GroupAnswers = this.GetGroupAnswers();
+            this.PersonAnswers = this.GetPersonAnswers();
         }
 
         public override long? ExecuteOne()
         {
-            throw new NotImplementedException();
+            var count = 0;
+            foreach (var group in this.GroupAnswers)
+            {
+                count += group.Count();
+            }
+            return count;
         }
 
         public override long? ExecuteTwo()
         {
-            throw new NotImplementedException();
+            var totalCount = 0;
+            foreach (var group in this.PersonAnswers)
+            {
+                var groupCount = 0;
+                foreach (var letter in "abcdefghijklmnopqrstuvwxyz")
+                {
+                    if (group.All(person => person.Contains(letter)))
+                    {
+                        groupCount++;
+                    }
+                }
+                totalCount += groupCount;
+            }
+            return totalCount;
+        }
+
+        private List<HashSet<char>> GetGroupAnswers()
+        {
+            var results = new List<HashSet<char>>();
+            var groups = this.Input.Trim().Split("\n\n");
+            foreach (var group in groups)
+            {
+                var groupAnswers = new HashSet<char>();
+                var persons = group.Trim().Split("\n");
+                foreach (var person in persons)
+                {
+                    foreach (var answer in person)
+                    {
+                        groupAnswers.Add(answer);
+                    }
+                }
+                results.Add(groupAnswers);
+            }
+            return results;
+        }
+
+        private List<List<HashSet<char>>> GetPersonAnswers()
+        {
+            var results = new List<List<HashSet<char>>>();
+            var groups = this.Input.Trim().Split("\n\n");
+            foreach (var group in groups)
+            {
+                var groupAnswers = new List<HashSet<char>>();
+                var persons = group.Trim().Split("\n");
+                foreach (var person in persons)
+                {
+                    var personAnswers = new HashSet<char>();
+                    foreach (var answer in person)
+                    {
+                        personAnswers.Add(answer);
+                    }
+                    groupAnswers.Add(personAnswers);
+                }
+                results.Add(groupAnswers);
+            }
+            return results;
         }
     }
 }
---
+++ AdventOfCode/AdventOfCode/Days/Day7.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode.Days
+{
+    public class Day7 : BaseDay
+    {
+        public static List<Bag> Bags { get; set; }
+
+        public Day7() : base(2020, 7)
+        {
+            Day7.Bags = new List<Bag>();
+
+            var lines = this.Input.Trim().Split("\n");
+            foreach (var line in lines)
+            {
+                var bag = new Bag(line);
+                Day7.Bags.Add(bag);
+            }
+
+            foreach (var bag in Day7.Bags)
+            {
+                bag.CalculateChildren();
+            }
+
+            foreach (var bag in Day7.Bags)
+            {
+                bag.CalculateParents();
+            }
+        }
+
+        public override long? ExecuteOne()
+        {
+            var shinyGold = Day7.Bags.FirstOrDefault(bag => bag.Colour == "shiny gold");
+            return shinyGold.GetAncestors().Count;
+        }
+
+        public override long? ExecuteTwo()
+        {
+            var shinyGold = Day7.Bags.FirstOrDefault(bag => bag.Colour == "shiny gold");
+            return shinyGold.GetDescendants();
+        }
+
+        [System.Diagnostics.DebuggerDisplay("{Colour}")]
+        public class Bag
+        {
+            public string Line { get; }
+
+            public string Colour { get; }
+
+            public Dictionary<Bag, int> Children { get; }
+
+            public Dictionary<Bag, int> Parents { get; }
+
+            public Bag (string line)
+            {
+                this.Line = line;
+                var parts = line.Split(" bags contain ");
+                this.Colour = parts[0].Replace(" bags", string.Empty);
+                this.Children = new Dictionary<Bag, int>();
+                this.Parents = new Dictionary<Bag, int>();
+            }
+
+            public void CalculateChildren()
+            {
+                var parts = Line.Split(" bags contain ");
+                var rules = parts[1]
+                    .Trim()
+                    .Split(",")
+                    .Select(part => part
+                        .Trim()
+                        .Replace(".", string.Empty)
+                        .Replace(" bags", string.Empty)
+                        .Replace(" bag", string.Empty));
+
+                foreach (var rule in rules)
+                {
+                    if (rule == "no other")
+                    {
+                        continue;
+                    }
+
+                    var numberString = rule.Substring(0, 1);
+                    if (!int.TryParse(numberString, out var number))
+                    {
+                        throw new InvalidProgramException("No valid number");
+                    }
+                    var colourString = rule.Substring(2);
+                    var otherBag = Day7.Bags.FirstOrDefault(bag => bag.Colour == colourString);
+                    if (otherBag == null)
+                    {
+                        throw new InvalidProgramException("No otherBag");
+                    }
+                    this.Children.Add(otherBag, number);
+                }
+            }
+
+            public void CalculateParents()
+            {
+                var otherBags = Day7.Bags.Where(bag => bag.Colour != this.Colour);
+                foreach (var otherBag in otherBags)
+                {
+                    foreach (var rule in otherBag.Children)
+                    {
+                        if (rule.Key == this)
+                        {
+                            this.Parents.Add(otherBag, rule.Value);
+                        }
+                    }
+                }
+            }
+
+            public HashSet<Bag> GetAncestors()
+            {
+                var ancestors = new HashSet<Bag>();
+                foreach (var parent in this.Parents)
+                {
+                    ancestors.Add(parent.Key);
+
+                    var grandParents = parent.Key.GetAncestors();
+                    ancestors.UnionWith(grandParents);
+                }
+                return ancestors;
+            }
+
+            public int GetDescendants()
+            {
+                var count = 0;
+                foreach (var child in this.Children)
+                {
+                    count += child.Value;
+                    count += child.Value * child.Key.GetDescendants();
+                }
+                return count;
+            }
+        }
+    }
+}
